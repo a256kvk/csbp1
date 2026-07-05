e93403c25ca16a6dac239daf9cbd366b8fd92126
antero <>
2026-07-05 23:17:53 +0300

    let the posts be longer

--- mysite/forms.py
+++ mysite/forms.py
@@ -3,11 +3,6 @@ from django import forms
 from .models import PostsModel, PrivateNotesModel
 
 class CreateForm(forms.Form):
-    content = forms.CharField(label="contents", max_length=100)
-    def save(self,user):
-        PostsModel.objects.create(account=user,content=self.cleaned_data["content"])
-
-class CreatePrivateNoteForm(forms.Form):
-    content = forms.CharField(label="contents", max_length=100)
+    content = forms.CharField(widget=forms.Textarea(), label="contents", max_length=1000)
     def save(self,user):
         PostsModel.objects.create(account=user,content=self.cleaned_data["content"])
